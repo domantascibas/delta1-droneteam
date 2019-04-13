7b8532de5f3c43603a25278130edaf0bb43ae2f7
Domantas <domantas.cibas@gmail.com>
2019-04-13 20:58:27 +0100

    progress

--- delta1-http.py
+++ delta1-http.py
@@ -1,4 +1,5 @@
 import serial
+import json
 
 port_name = '/dev/ttyAMA0'
 port_baudrate = 115200
@@ -9,7 +10,15 @@ longitude = 24.371699
 RESPONSE_LEN = 128
 
 g = serial.Serial(port_name, port_baudrate, timeout=2)
-g.write('AT+HTTPPARA="URL",', domain_name)
+g.write('AT+CGATT=1')
+
+g.write('AT+HTTPINIT')
+print g.read(RESPONSE_LEN)
+
+g.write('AT+HTTPPARA="CID",1')
+print g.read(RESPONSE_LEN)
+
+g.write('AT+HTTPPARA="URL","http://vilniustransport.com"')
 print g.read(RESPONSE_LEN)
 
 g.write('AT+HTTPPARA="CONTENT","application/json"')
