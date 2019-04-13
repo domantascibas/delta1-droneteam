682a67701dd432b16f0f29dbeeeb9d8c05c1b276
Domantas Cibas <domantas.cibas@ruptela.com>
2019-04-13 15:07:26 +0300

    adds gsm class

--- delta1-gsm.py
+++ delta1-gsm.py
@@ -1,15 +1,59 @@
 import serial
 
-ser = serial.Serial('/dev/ttyAMA0', 115200)
-ser.close()
+port_name = '/dev/ttyAMA0'
+port_baudrate = 115200
 
-ser.open()
-ser.write("AT")
+class GsmModule():
 
-serial_line = ser.readline()
-print serial_line
-while(serial_line != ''):
-  serial_line = ser.readline()
-  print serial_line
+  def __init__(self, port, baud):
+    self.port = port
+    self.baud = baud
+    self.serial = serial.Serial(self.port, self.baud)
+    self.serial.close()
 
-ser.close()
+  def openSerial(self):
+    self.serial.open()
+
+  def closeSerial(self):
+    self.serial.close()
+
+  def sendCommand(self, cmd):
+    self.openSerial()
+    self.serial.write(cmd + '\r')
+
+    rcv = ''
+
+    while(self.serial.read() != ''):
+      rcv += self.serial.read()
+
+    self.closeSerial()
+    print rcv
+
+Gsm = GsmModule(port_name, port_baudrate)
+Gsm.sendCommand("AT")
+
+# ser = serial.Serial('/dev/ttyAMA0', 115200)
+# ser.close()
+
+# ser.open()
+# ser.write("AT")
+
+# serial_line = ser.readline()
+# print serial_line
+# while(serial_line != ''):
+#   serial_line = ser.readline()
+#   print serial_line
+
+# ser.close()
+
+# def send_AT_cmd(cmd):
+#   port = serial.Serial(port_name, port_baudrate)
+#   port.write(cmd + '\r')
+  
+#   rcv = ''
+
+#   while(port.read() != ''):
+#     rcv += port.read()
+
+#   port.close()
+#   print rcv
